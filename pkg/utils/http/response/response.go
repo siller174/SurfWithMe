f4523a6da69c2da5a746ea44368e8423d5843337
Sergei <siller174@gmail.com>
2020-04-17 19:45:29 +0300

    fix cors

--- pkg/utils/http/response/response.go
+++ pkg/utils/http/response/response.go
@@ -9,7 +9,7 @@ import (
 func WriteJSON(w http.ResponseWriter, code int, message []byte) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD")
 	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Header().Set("Vary", "Accept-Encoding")
