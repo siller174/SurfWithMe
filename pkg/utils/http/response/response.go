7a29ab4094ee0280b1b70b340c7614482b6129dc
Sergei <siller174@gmail.com>
2020-04-17 18:54:38 +0300

    fix cors

--- pkg/utils/http/response/response.go
+++ pkg/utils/http/response/response.go
@@ -1,12 +1,18 @@
 package response
 
 import (
-	"github.com/siller174/meetingHelper/pkg/logger"
 	"net/http"
+
+	"github.com/siller174/meetingHelper/pkg/logger"
 )
 
 func WriteJSON(w http.ResponseWriter, code int, message []byte) error {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Vary", "Accept-Encoding")
 	w.WriteHeader(code)
 	writeBytes, err := w.Write(message)
 	if err != nil {
@@ -16,13 +22,14 @@ func WriteJSON(w http.ResponseWriter, code int, message []byte) error {
 	if writeBytes == 0 {
 		logger.Error("Wrote empty message in response")
 	}
+
 	return nil
 }
 
-func Empty(w http.ResponseWriter)  {
+func Empty(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func NotFound(w http.ResponseWriter)  {
+func NotFound(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNotFound)
 }
