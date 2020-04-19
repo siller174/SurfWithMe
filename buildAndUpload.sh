25fb6ec5ed53529b844de6df8f7b389fb38d3487
Sergei <siller174@gmail.com>
2020-04-19 14:44:01 +0300

    fix sccripts

--- buildAndUpload.sh
+++ buildAndUpload.sh
@@ -2,3 +2,8 @@
 
 docker image build -t meetingunit:0.2 . ;
 docker save meetingunit:0.2 | ssh root@178.128.165.236 docker load
+ssh root root@178.128.165.236 docker stop meetingunit
+ssh root root@178.128.165.236 docker rm meetingunit
+ssh root root@178.128.165.236 docker run -d -p 9091:8080 --link=redis_cont --name meetingunit meetingunit
+
+
--- runRedis.sh
+++ runRedis.sh
@@ -3,6 +3,6 @@
 docker run  --rm -d \
             -p 6379:6379 \
 	        -e REDIS_PASSWORD=hardpassword \
-    	    -v $PWD/redis-data:/bitnami/redis/data \
+    	    -v /opt/redis-data:/bitnami/redis/data \
     	    --name redis \
     	    bitnami/redis:latest
