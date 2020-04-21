ddd64e90f2f6e410002fb959d375a60115d57c32
Sergei <siller174@gmail.com>
2020-04-21 19:57:21 +0300

    fix cors

--- pkg/meetingHelper/api/cors.go
+++ pkg/meetingHelper/api/cors.go
@@ -7,6 +7,7 @@ import (
 func AddCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, X-Requested-With, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
 		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0") // TODO CHECK
 		w.Header().Set("Vary", "Accept-Encoding") // TODO CHECK
